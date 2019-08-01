cef7421d07732df2b319bf17986ebd3efd176af6
domreader <dominic.reader@students.plymouth.ac.uk>
2019-08-01 13:47:09 +0100

    Creating Project and adding movement script

---
+++ AINT254/Floatering/Assets/Scripts/playerMovement.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+
+    CharacterController characterController;
+
+    public int score;
+    public int updatedScore;
+
+    public float movementSpeed = 6.0f;
+    public float floating = 0.5f;
+    public float gravity = 10f;
+
+   
+
+    private Vector3 directionalMovement = Vector3.zero;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        score = 10;
+
+        float moveUP = floating + 5.0f;
+
+        characterController = GetComponent<CharacterController>();
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float moveUP = floating + 5.0f;
+
+        if (characterController.isGrounded)
+        {
+            score = (score - 1);
+        }
+
+        if (Input.GetButton("Jump") == true)
+        {
+            directionalMovement.y = moveUP;
+        }
+
+        directionalMovement = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+
+       // directionalMovement.y -= gravity * Time.deltaTime;
+
+        characterController.Move(directionalMovement * Time.deltaTime);
+        
+
+
+    }
+
+    void FixedUpdate()
+    {
+        
+    }
+}
---
+++ AINT254/Floatering/Assets/Scripts/scoreCalculator.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scoreCalculator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int score;
+        int updatedScore;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+
+        
+
+    }
+
+
+   
+
+}
